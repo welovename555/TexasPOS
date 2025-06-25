d7fb4c6f9d9d4da6ff561c04e4301bd4db2b66a6
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-25 16:04:25 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
 const SUPABASE_URL = 'https://jkenfjjxwdckmvqjkdkp.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprZW5mamp4d2Rja212cWprZGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjA5NjIsImV4cCI6MjA2NjM5Njk2Mn0.3VOZpt4baNnC5-qpYq6dC9UZ0gcfI2V4aTdi1itxmXI';
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export { client as supabaseClient };
