a6444b49f25a3cc66a8ac7b3864385e0d3769d51
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-25 14:53:04 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://cgulsibwftrpshwwmnpc.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNndWxzaWJ3ZnRycHNod3dtbnBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NDQ1MTQsImV4cCI6MjA2NjIyMDUxNH0.8sjb4UeHQZTm7LxcD1UREDfRH20EKzIJnQLNaUSesmU';
+const SUPABASE_URL = 'https://jkenfjjxwdckmvqjkdkp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprZW5mamp4d2Rja212cWprZGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjA5NjIsImV4cCI6MjA2NjM5Njk2Mn0.3VOZpt4baNnC5-qpYq6dC9UZ0gcfI2V4aTdi1itxmXI';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 export { supabase };
