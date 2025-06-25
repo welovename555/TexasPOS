0967b66e0921f0aa48a0dcf42312f52dd7f6b7d4
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-25 15:50:12 +0700

    Update cartStore.js

--- js/stores/cartStore.js
+++ js/stores/cartStore.js
@@ -5,12 +5,37 @@ const cartStore = {
   },
 
   addItem(product) {
-    // Logic will be implemented later
-    console.log(`(cartStore) Adding item: ${product.name}`);
+    const existingItem = this.state.items.find(item => item.product.id === product.id);
+
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      this.state.items.push({ product: product, quantity: 1 });
+    }
+    this.updateTotal();
+  },
+
+  removeItem(productId) {
+    const itemIndex = this.state.items.findIndex(item => item.product.id === productId);
+
+    if (itemIndex > -1) {
+      const item = this.state.items[itemIndex];
+      if (item.quantity > 1) {
+        item.quantity--;
+      } else {
+        this.state.items.splice(itemIndex, 1);
+      }
+      this.updateTotal();
+    }
   },
 
   updateTotal() {
-    // Logic will be implemented later
+    const total = this.state.items.reduce((sum, item) => {
+      return sum + (item.product.base_price * item.quantity);
+    }, 0);
+    this.state.total = total;
+    
+    document.dispatchEvent(new CustomEvent('cartUpdated', { detail: this.state }));
   },
 
   getCart() {
@@ -20,6 +45,6 @@ const cartStore = {
   clearCart() {
     this.state.items = [];
     this.state.total = 0;
-    console.log('(cartStore) Cart cleared.');
+    document.dispatchEvent(new CustomEvent('cartUpdated', { detail: this.state }));
   }
 };
