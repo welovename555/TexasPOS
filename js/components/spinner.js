a31f08b8c96e19963a1a8e8b77fa446ba87686a8
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-25 16:20:48 +0700

    Update spinner.js

--- js/components/spinner.js
+++ js/components/spinner.js
@@ -5,28 +5,17 @@ const Spinner = {
       spinnerOverlay = document.createElement('div');
       spinnerOverlay.id = 'spinner-overlay-dynamic';
       spinnerOverlay.className = 'spinner-overlay';
-
-      const spinnerElement = document.createElement('div');
-      spinnerElement.className = 'spinner';
-      spinnerOverlay.appendChild(spinnerElement);
-
+      spinnerOverlay.innerHTML = `<div class="spinner"></div>`;
       document.body.appendChild(spinnerOverlay);
     }
-
-    setTimeout(() => {
-        spinnerOverlay.classList.add('active');
-    }, 10);
+    setTimeout(() => spinnerOverlay.classList.add('active'), 10);
   },
-
   hide() {
     const spinnerOverlay = document.getElementById('spinner-overlay-dynamic');
     if (spinnerOverlay) {
       spinnerOverlay.classList.remove('active');
-      setTimeout(() => {
-        if (document.body.contains(spinnerOverlay)) {
-          document.body.removeChild(spinnerOverlay);
-        }
-      }, 200);
+      setTimeout(() => { if (document.body.contains(spinnerOverlay)) { document.body.removeChild(spinnerOverlay); } }, 200);
     }
   }
 };
+export { Spinner };
