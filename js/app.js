1556b2e51a593e434c392b67f2c25e4809825ed7
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-26 21:09:49 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 import { sellView } from './views/sellView.js';
 import { cartView } from './views/cartView.js';
 import { checkoutModal } from './components/checkoutModal.js';
+import { priceSelectorModal } from './components/priceSelectorModal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Initialize the main views
   sellView.init();
-  cartView.init('#cart-view-container');
+  // The cartView is not used in the final design, but keeping the line commented
+  // in case you want to reuse it later.
+  // cartView.init('#cart-view-container');
+
+  // Global event listener to open the price selector modal
+  window.addEventListener('openPriceSelector', (e) => {
+    const { product } = e.detail;
+    if (product) {
+      priceSelectorModal.open(product);
+    }
+  });
 });
