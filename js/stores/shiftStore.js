213d4e76f898e896a9ec079190f14dd54bb28c6f
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-27 07:37:42 +0700

    Update shiftStore.js

--- js/stores/shiftStore.js
+++ js/stores/shiftStore.js
@@ -8,16 +8,22 @@ const shiftStore = {
 
   init() {
     const shiftSession = sessionStorage.getItem(SHIFT_SESSION_KEY);
+    console.log('[DEBUG] shiftStore.init(): Found session data:', shiftSession);
     if (shiftSession) {
       this.state.currentShift = JSON.parse(shiftSession);
       this.state.isActive = true;
+      console.log('[DEBUG] shiftStore.init(): State updated. isActive:', this.state.isActive);
+    } else {
+      console.log('[DEBUG] shiftStore.init(): No session data found.');
     }
   },
 
   setShift(shiftData) {
+    console.log('[DEBUG] shiftStore.setShift(): Setting new shift data:', shiftData);
     this.state.currentShift = shiftData;
     this.state.isActive = true;
     sessionStorage.setItem(SHIFT_SESSION_KEY, JSON.stringify(shiftData));
+    console.log('[DEBUG] shiftStore.setShift(): Data saved to sessionStorage.');
   },
 
   clearShift() {
