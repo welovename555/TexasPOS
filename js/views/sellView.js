808c7974f7969c27497908fe8e9224d98c295af6
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-07-01 07:18:02 +0700

    Update sellView.js

--- js/views/sellView.js
+++ js/views/sellView.js
@@ -149,7 +149,7 @@ const sellView = {
     const placeholderSVG = `
         <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <rect width="100" height="100" fill="rgba(255, 255, 255, 0.0)"/>
-            <g transform="translate(38, 38) scale(1.2)">
+            <g transform="translate(38, 38) scale(2.0)">
                 ${iconSVG}
             </g>
         </svg>
